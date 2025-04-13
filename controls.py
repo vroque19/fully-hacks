487184ba24575770e87e660d461f0f0b6a77c193
Bazil Alvarez <bazilalvarez@gmail.com>
2025-04-13 01:59:10 -0700

    y axis works

--- controls.py
+++ controls.py
@@ -1,59 +1,11 @@
-from gpiozero import OutputDevice
-import keyboard
-import time
+from gpiozero import Button
 
-STEP_SEQUENCE = [
-        [1, 0, 0, 1],
-        [1, 0, 0, 0],
-        [1, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 1],
-        [0, 0, 0, 1]
-    ]
-def rotate_y_counter():
-  control_pins = [17, 18, 27, 22]
-  pins = [OutputDevice(pin) for pin in control_pins]
-  for halfstep in reversed(STEP_SEQUENCE):  # reverse the sequence
-      for pin in range(4):
-          pins[pin].value = halfstep[pin]
-      time.sleep(0.001)
 
-def rotate_y_clockwise():
-  control_pins = [17, 18, 27, 22]
-  pins = [OutputDevice(pin) for pin in control_pins]
-  steps = 64 # 45 degrees
-  for halfstep in STEP_SEQUENCE:  # reverse the sequence
-      for pin in range(4):
-          pins[pin].value = halfstep[pin]
-      time.sleep(0.001)
+forward = Button(5, pull_up=False)
+backward = Button(6, pull_up=False)
 
-# def rotate_x_counter():
-#   control_pins = [23, 24, 25, 8]
-#   pins = [OutputDevice(pin) for pin in control_pins]
-#   for halfstep in reversed(STEP_SEQUENCE):  # reverse the sequence
-#       for pin in range(4):
-#           pins[pin].value = halfstep[pin]
-#       time.sleep(0.001)
-
-# def rotate_x_clockwise():
-#   control_pins = [23, 24, 25, 8]
-#   pins = [OutputDevice(pin) for pin in control_pins]
-#   steps = 64 # 45 degrees
-#   for halfstep in STEP_SEQUENCE:  # reverse the sequence
-#       for pin in range(4):
-#           pins[pin].value = halfstep[pin]
-#       time.sleep(0.001)
-
-def main():
-    # while True:
-    #     if keyboard.is_pressed("up"):
-    #         rotate_y_counter()
-    #     if keyboard.is_pressed("down"):
-    #         rotate_y_clockwise()
-    rotate_y_clockwise()
-    rotate_y_counter()
-
-if __name__ == "__main__":
-    main()
+while True:
+  if forward.is_pressed:
+    print("forward pressed")
+  if backward.is_pressed:
+    print("backward pressed")
